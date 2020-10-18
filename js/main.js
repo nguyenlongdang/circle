a6fc3446ecf01b41a3ac774d52771aec3a0df56b
Nguyen Long Dang <longdang0412@gmail.com>
2020-10-18 20:37:34 +0700

    Ecommerce Add Detail Page

--- js/main.js
+++ js/main.js
@@ -60,7 +60,7 @@ $(function () {
   });
 
   // modal slider
-  var galleryThumbs = new Swiper('.gallery-thumbs', {
+  var galleryThumbs = new Swiper(".gallery-thumbs", {
     observer: true,
     observeParents: true,
     spaceBetween: 10,
@@ -69,13 +69,53 @@ $(function () {
     watchSlidesVisibility: true,
     watchSlidesProgress: true,
   });
-  var galleryTop = new Swiper('.gallery-top', {
+  var galleryTop = new Swiper(".gallery-top", {
     observer: true,
     observeParents: true,
     spaceBetween: 10,
     thumbs: {
-      swiper: galleryThumbs
+      swiper: galleryThumbs,
+    },
+  });
+
+  // detail slider
+  var detailGalleryThumbs = new Swiper(".detail-gallery-thumbs", {
+    spaceBetween: 10,
+    slidesPerView: 4,
+    freeMode: true,
+    watchSlidesVisibility: true,
+    watchSlidesProgress: true,
+  });
+  var detailGalleryTop = new Swiper(".detail-gallery-top", {
+    spaceBetween: 10,
+    thumbs: {
+      swiper: detailGalleryThumbs,
+    },
+  });
+
+  // venobox detail
+  $(".venobox").venobox({
+    numeratio: true,
+    border: "20px",
+  });
+
+  $(".accordion__title").click(function () {
+    const $accordion_wrapper = $(this).parent();
+    const $accordion_content = $(this)
+      .parent()
+      .find(".accordion__content")
+      .first();
+    const $accordion_open = "accordion--open";
+
+    // If this accordion is already open
+    if ($accordion_wrapper.hasClass($accordion_open)) {
+      $accordion_content.slideUp(); // Close the content.
+      $accordion_wrapper.removeClass($accordion_open); // Remove the accordionm--open class.
+    }
+    // If this accordion is not already open
+    else {
+      $accordion_content.slideDown(); // Show this accordion's content.
+      $accordion_wrapper.addClass($accordion_open); // Add the accordion--open class.
     }
   });
 });
-
